ee6c030d873633c69afd384754d80ed5a4cbe708
24paketik <deniska-smirnov-05@mail.ru>
2022-04-29 13:59:43 +0300

    Create

---
+++ Gl6_pr6.5/Company.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gl6_pr6._5
+{
+    public class Company
+    {
+        string name; //название
+        int persons; //количество сотрудников
+        int money; // месячный фонд заработной платы
+        public string Name
+        {// свойство
+            get { return name; }
+        }
+        public int Persons
+        {// свойство
+            get { return persons; }
+        }
+        public Company(
+        string name, int persons, int money)
+        {
+            this.name = name; this.persons = persons;
+            this.money = money;
+        }
+        // печать информации о компании
+        public void show()
+        {
+            Console.WriteLine("В компании " + name + " трудятся " + persons + " сотрудников ");
+            Console.WriteLine("Фонд зарплаты: " + money);
+        }
+    }
+}
---
+++ Gl6_pr6.5/Count.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gl6_pr6._5
+{
+    public class Count: IComparer<Company>
+    {
+        public int Compare(Company x, Company y)
+        {
+            if (x.Persons > y.Persons)
+                return 1;
+            else if (x.Persons < y.Persons)
+                return -1;
+            else return 0;
+        }
+    }
+}
